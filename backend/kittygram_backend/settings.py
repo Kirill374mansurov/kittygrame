4596ed93dac288f4c226dbfd0ea9712b56e2c15d
Кирилл Мансуров <kirill.mansurov.2025@mail.ru>
2024-09-04 18:11:24 +0300

    test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', 'True').lower
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost/127.0.0.1').split('/')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kittygramkirill.zapto.org', os.getenv('ALLOWED_HOSTS')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
